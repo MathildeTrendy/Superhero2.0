9198585d7207bc1ea25c3c2b78205452df716e39
mathildetrend <math38v6@stud.kea.dk>
2022-11-04 15:14:23 +0100

    Superhelte

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -5,14 +5,6 @@ public class Database {
 
     private ArrayList<Superhero> superheroes = new ArrayList<Superhero>(List.of());
 
-    public void createTestData() {
-        createSuperhero("Bobbel", "Samme som heltenavn", 1998, "Tale med dyr", false);
-        createSuperhero("Superman", "Clark Kent", 1978, "Flyve", false);
-        createSuperhero("Kaptajn Underhyler","Knold", 2017, "Flyve", true);
-        createSuperhero("Robin","Jason Todd", 1823, "Super styrke", true);
-        createSuperhero("Bolt", "Samme som heltenavn", 2008, "Lasersyn", false);
-
-    }
 
     public void createSuperhero(String heltenavn, String ægtenavn, int årstal, String superkraft, boolean human) {
 
