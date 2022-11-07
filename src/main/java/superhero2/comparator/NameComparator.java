b8d8ee565724e4532b83549e5602ea18c81bc0ed
najamoe <naja0718@stud.kea.dk>
2022-11-07 15:00:34 +0100

    CSV file and load working

--- src/main/java/superhero2/comparator/NameComparator.java
+++ src/main/java/superhero2/comparator/NameComparator.java
@@ -3,7 +3,6 @@ import superhero2.Superhero;
 
 import java.util.Comparator;
 
-
     public class NameComparator implements Comparator<Superhero> {
         public int compare( Superhero a1, Superhero a2) {
             return a1.getÆgteNavn().compareToIgnoreCase(a2.getÆgteNavn());
