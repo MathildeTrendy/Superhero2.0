13ac87ccefafaf63466197e20ebfbdf2b4d8f75c
gottbrecht <simo79j5@stud.kea.dk>
2022-11-07 16:06:06 +0100

    Strength

--- src/main/java/superhero2/comparator/Strength.java
+++ src/main/java/superhero2/comparator/Strength.java
@@ -1,14 +1,10 @@
 package superhero2.comparator;
-
 import superhero2.Superhero;
-
 import java.util.Comparator;
 
-public class Strength {
-
-    public class Strength implements Comparator<Superhero> {
-        public int compare( Superhero a1, Superhero a2) {
-            return a1.get().compareToIgnoreCase(a2.getÆgteNavn());
+public class Strength implements Comparator<Superhero> {
+        public int compare(Superhero a1, Superhero a2) {
+            return Double.compare(a1.getStrength(), a2.getStrength());
         }
     }
-}
+
